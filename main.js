9bf312878b7bee317ff2f1db30a3018b7b8c9163
Jip <jipfrijlink@gmail.com>
2020-01-25 17:13:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+const url = `https://api.giphy.com/v1/gifs/search?api_key=1wwNNO66hKokAFyXJQ7JJAkPNsz8Ibxp&limit=25&offset=0`
+
+function search(q) {
+	fetch(`${url}&q=swear_trek ${q}`).then(d => d.json()).then(d => {
+		
+		let results = d.data.filter(result => result.username === "Swear_Trek");
+		console.log(results);
+		
+		document.querySelector(".results").innerHTML = 
+			results.map(result => `<img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="${result.images.original.url}">`).join("");
+
+		next_img();
+
+	});
+}
+
+function next_img() {
+	let img = document.querySelector("[data-src]");
+	if(!img) return;
+	img.src = img.dataset.src;
+	img.removeAttribute("data-src");
+	img.onload = next_img;
+}
+
+function init() {
+	document.querySelector(".search").addEventListener("input", evt => {
+		search(evt.currentTarget.value);
+	});
+	search("");
+}
+
+window.addEventListener("load", init);
